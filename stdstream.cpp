5191fdd4ad29c2369a355ebe980944947311b65b
chordncode <chordncode@gmail.com>
2023-08-17 21:01:43 +0900

    first commit

---
+++ stdstream.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void){
+
+	char a[100], b[100];
+	std::cout << "Input String : ";
+	std::cin >> a >> b;
+
+	std::cout << "value from a : " << a << std::endl;
+	std::cout << "value from b : " << b << std::endl;
+
+	return 0;
+}
